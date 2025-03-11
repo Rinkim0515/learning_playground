cbff0b73e3dbf929715211870638f7c2a2dcd9c6
Rinkim0515 <kimrindev@gmail.com>
2025-03-11 14:17:08 +0900

    R class

---
+++ R/class_r/temptation.R
@@ -0,0 +1,19 @@
+a <-1
+b <-3
+a+b
+x <-c(1:10)
+plot(11:20,x)
+getwd()
+setwd('/Users/bloom/Desktop/learning_playground/R/class_r')
+getwd()
+
+x <-c(1,2,3,4,5)
+y <-c(10,20,30,40,50)
+dat <-cbind(x,y)
+dat
+1
+w2 <- scan()
+
+dat3 <- data.frame()
+dat3 = edit(dat3)
+
